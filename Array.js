47ccc5d46f20c8c3a2b5991b8a248f710b90dcbd
Captain-077 <nakuldesigner77@gmail.com>
2022-09-28 18:08:28 +0530

    triangle exercise

---
+++ Array.js
@@ -0,0 +1,20 @@
+const numberArray = [1, 3, 6, 7]; // Number array
+console.log('number array', numberArray);
+
+const stringArray = ['Pawan', 'Pervez', 'Pradeep', 'Nakul', 'Pawan']
+console.log('string based array', stringArray)
+
+const booleanArray = [false, false, true, true, false, true];
+console.log('boolean based array', booleanArray);
+
+const arrayOfArray = [
+    [2, 4, 6],
+    [3, 6, 9]
+];
+console.log('Array of array', arrayOfArray);
+
+const arrayOfObject = [{ name: 'pawan' }, { name: 'nakul' }];
+console.log('array of object', arrayOfObject);
+
+const mixedArray = ['Pawan', 30, true, ['music', 'cricket']];
+console.log('mixed array', mixedArray);
